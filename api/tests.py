838774ab9f501be3ad18f4e916d0ae34fe847256
Suhail <suhailsid@student.iul.ac.in>
2024-10-31 18:59:58 +0530

    updated

--- api/tests.py
+++ api/tests.py
@@ -6,7 +6,7 @@ from .models import Employee
 class EmployeeTests(APITestCase):
     def test_create_employee(self):
         url = reverse('employee-list-create')
-        data = {'name': 'John Doe', 'email': 'john@example.com'}
+        data = {'name': 'Suhail Siddiqui', 'email': 'suhailsiddiqui1530@gmail.com'}
         response = self.client.post(url, data, format='json')
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
@@ -16,20 +16,20 @@ class EmployeeTests(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def test_retrieve_employee(self):
-        employee = Employee.objects.create(name='John Doe', email='john@example.com')
+        employee = Employee.objects.create(name='Suhail Siddiqui', email='suhailsiddiqui1530@gmail.com')
         url = reverse('employee-detail', args=[employee.id])
         response = self.client.get(url, format='json')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def test_update_employee(self):
-        employee = Employee.objects.create(name='John Doe', email='john@example.com')
+        employee = Employee.objects.create(name='Suhail Siddiqui', email='suhailsiddiqui1530@gmail.com')
         url = reverse('employee-detail', args=[employee.id])
-        data = {'name': 'John Smith'}
+        data = {'name': 'Siddiqui Suhail'}
         response = self.client.put(url, data, format='json')
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.status_code, status.HTTP_200_OK) 
 
     def test_delete_employee(self):
-        employee = Employee.objects.create(name='John Doe', email='john@example.com')
+        employee = Employee.objects.create(name='Suhail', email='suhailsiddiqui1530@gmail.com')
         url = reverse('employee-detail', args=[employee.id])
         response = self.client.delete(url)
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
